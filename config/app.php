1e422654c672c83b760220d8d8d7ddd5979b3f28
wahyu909 <151610097@smkassalaambandung.sch.id>
2017-08-03 12:33:36 +0700

    Larapus 	new file:   public/fonts/fontawesome-webfont.svg

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => 'Larapus',
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        //Add Plus
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -225,6 +227,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        //Add Class
+        'Form'=> Collective\Html\FormFacade::class,
+        'Html'=> Collective\Html\FormFacade::class,
+        'Laratrust' => Laratrust\LaratrustFacades::class,
 
     ],
 
